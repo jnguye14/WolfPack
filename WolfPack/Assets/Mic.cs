d6265bd0c61bb77841cfd60ffaaabf2edac63d2d
Jordan Nguyen <Jordan.N.Nguyen@asu.edu>
2014-02-11 18:04:11 -0700

    Unity Project

---
+++ WolfPack/Assets/Mic.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using UnityEditor; // EditorUtility
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Mic : MonoBehaviour
+{
+	public string microphoneName = "Built-in Microphone";
+	public bool hasPermission = false;
+	public KeyCode startKey = KeyCode.Space;
+	public KeyCode stopKey = KeyCode.Backspace;
+	public KeyCode playKey = KeyCode.Return;
+	public KeyCode saveKey = KeyCode.Tab;
+
+	// Use this for initialization
+	IEnumerator Start () {
+	//void Start(){
+//# if UNITY_WEBPLAYER
+		// Request permission to use both webcam and microphone.
+		yield return Application.RequestUserAuthorization (UserAuthorization.Microphone);
+
+		if (true || Application.HasUserAuthorization(UserAuthorization.Microphone))
+		{
+			// we got permission. Set up microphone.
+			hasPermission = true;
+
+			foreach(string device in Microphone.devices)
+			{
+				Debug.Log("Name: " + device);
+			}
+			microphoneName = Microphone.devices[0]; /*"Built-in Microphone"*/
+		}
+		else
+		{
+			// no permission. Show error here.
+			hasPermission = false;
+			Debug.Log("Need permission to use microphone");
+		}
+//#endif
+		yield return null;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//*
+		if(Microphone.IsRecording(microphoneName))
+		{
+			Debug.Log ("recording");
+		}//*/
+
+		if (Input.GetKeyDown (startKey))
+		{
+			StartRecording();
+		}
+		if (Input.GetKeyDown (stopKey))
+		{
+			StopRecording();
+		}
+		if (Input.GetKeyDown (playKey))
+		{
+			PlayRecording(false);	
+		}
+		if (Input.GetKeyDown (saveKey))
+		{
+			SaveRecording("myFile");	
+		}
+	}
+
+	void StartRecording()
+	{
+		if (!hasPermission)
+		{
+			Debug.Log ("No permission to use microphone");
+			return;
+		}
+
+		if (!Microphone.IsRecording (microphoneName))
+		{
+			audio.clip = Microphone.Start (microphoneName, false, 10, 44100);
+			// micName, loopRecording, numSecs, freq);
+		}
+		else
+		{
+			Debug.Log ("already recording");
+		}
+	}
+
+	void StopRecording()
+	{
+		if (!hasPermission)
+		{
+			Debug.Log ("No permission to use microphone");
+			return;
+		}
+
+		if (Microphone.IsRecording (microphoneName))
+		{
+			Microphone.End (microphoneName);
+		}
+		else
+		{
+			Debug.Log ("not recording");
+		}
+	}
+
+	void PlayRecording(bool looping)
+	{
+		if (!hasPermission)
+		{
+			Debug.Log ("No permission to use microphone");
+		}
+
+		if (!Microphone.IsRecording (microphoneName) && !audio.isPlaying)
+		{
+			audio.loop = looping;
+			audio.Play ();
+		}
+		else
+		{
+			Debug.Log ("Unable to play recording");
+		}
+	}
+
+	void SaveRecording(string filename)
+	{
+		if (!hasPermission)
+		{
+			Debug.Log ("No permission to use microphone");
+		}
+
+		if (!Microphone.IsRecording (microphoneName))
+		{
+			//filename += System.DateTime.Now.ToString("MM-dd-yy_hh-mm-ss") +".ogg";
+			//var filepath = Path.Combine(Application.dataPath, filename);
+			//EditorUtility.ExtractOggFile(audio.clip, filepath);
+			SavWav.Save (filename, audio.clip);
+		}
+		else
+		{
+			Debug.Log ("unable to save, still recording");
+		}
+	}
+}
---
+++ WolfPack/Assets/script.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class script: MonoBehaviour
+{
+	private const int listenPort = 29129;
+	TcpClient tcpclnt;
+	NetworkStream stm;
+	//IPEndPoint groupEP;
+
+	void Start()
+	{
+		try
+		{
+			Debug.Log ("Connecting");
+			tcpclnt = new TcpClient ();
+			tcpclnt.Connect(System.Net.Dns.GetHostName(),listenPort);
+			stm = tcpclnt.GetStream();
+		}
+		catch(Exception e)
+		{
+			Debug.Log("Error: " + e.StackTrace);
+		}
+		Debug.Log ("Connected");
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("Clicked");		
+		}
+
+		if (tcpclnt.Connected && Input.GetButtonDown("Jump")) // left click
+		{
+			Debug.Log ("Pressed Space; Encoding Data");
+			ASCIIEncoding asen= new ASCIIEncoding();
+			byte[] ba = asen.GetBytes("GetInput");
+			//byte[] data = new byte[str.Length * sizeof(char)];
+			//System.Buffer.BlockCopy(str.ToCharArray(), 0, data, 0, data.Length);
+
+			Debug.Log("Transmitting.....");
+			stm.Write(ba, 0, ba.Length);
+
+			//*
+			byte[] bb = new byte[100];
+			int k = stm.Read(bb, 0, 100);
+			string op = asen.GetString(bb);
+			string[] words = op.Split(' ');
+			Debug.Log (op);
+			//*/
+
+			//tcpclnt.Close();
+		}
+		else if(!tcpclnt.Connected)
+		{
+			Debug.Log("need to connect");
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnDestroy()
+	{
+		if(tcpclnt.Connected)
+		{
+			tcpclnt.Close();
+		}
+	}
+}
